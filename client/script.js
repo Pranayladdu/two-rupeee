014f0cd0e0a708db9cee66d5323322995daac2ee
photographypk598-max <photographypk594@gmail.com>
2025-11-13 19:48:49 +0530

    first commit

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "https://two-rupeee-6.onrender.com";
 
 const amountEl = document.getElementById("amount");
 const userNameEl = document.getElementById("userName");
